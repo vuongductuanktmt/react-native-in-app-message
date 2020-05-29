b675c0dad49c69f86b3c1b0d4e9bb4b4294419fa
vuongductuanktmt <anhtuanpro312@gmail.com>
2020-05-29 22:20:30 +0700

    Update Notification.android.js

--- src/Noification/Notification.android.js
+++ src/Noification/Notification.android.js
@@ -1,76 +1,100 @@
 import React, { Fragment } from 'react';
-import { Animated, View, Text, TouchableOpacity, StatusBar } from 'react-native';
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import {
+  Animated,
+  View,
+  Text,
+} from 'react-native';
+import {
+  PanGestureHandler,
+  TapGestureHandler
+} from 'react-native-gesture-handler';
 import { NotificationBase } from './NotificationBase';
 import { androidStyle } from "./androidStyle";
 
-const minVelocityToFling = -250;
+const MIN_VELOCITY_TO_FLING = -350;
+const BOUNCE_OFFSET = 150;
 
 export class Notification extends NotificationBase {
 
-	static defaultProps = {
-		duration: 2000,
-		autohide: true,
-    blurAmount: 7,
-    showKnob: true
-	};
+  static defaultProps = {
+    duration: 2000,
+    autohide: true,
+  };
 
-	offset = StatusBar.currentHeight;
+  offset = 0;
 
-	onHandlerStateChange = (event) => {
-		const {velocityY, translationY, numberOfPointers, state} = event.nativeEvent;
+  onHandlerStateChange = (event) => {
+    const {velocityY, translationY, numberOfPointers, state} = event.nativeEvent;
 
-		if (this.props.onDragGestureHandlerStateChange) {
-			this.props.onDragGestureHandlerStateChange(event);
-		}
+    if (this.props.onDragGestureHandlerStateChange) {
+      this.props.onDragGestureHandlerStateChange(event);
+    }
 
-		if (state === 5) {
-			if (velocityY < minVelocityToFling && numberOfPointers === 0) {
-				Animated.spring(this.translateY, {
-					toValue: (this.viewHeight + this.offset * 2) * -1,
-					useNativeDriver: true,
-					velocity: velocityY,
-				}).start();
-				return;
-			}
+    if (state === 5) {
+      if (velocityY < MIN_VELOCITY_TO_FLING && numberOfPointers === 1) {
+        Animated.spring(this.translateY, {
+          toValue: (this.viewHeight + BOUNCE_OFFSET + this.offset * 2) * -1,
+          useNativeDriver: true,
+          velocity: velocityY,
+        }).start();
+        return;
+      }
 
-			if (translationY > ((this.viewHeight / 2) * -1)) {
-				this.show();
-			} else {
-				this.hide();
-			}
-		}
-	};
+      if (translationY > ((this.viewHeight / 2) * -1)) {
+        this.show();
+      } else {
+        this.hide();
+      }
+    }
+  };
 
-	renderOwnComponent() {
-		const {textColor, text} = this.props;
-		return <Text style={[androidStyle.text, {color: textColor}]}>{text}</Text>;
-	}
+  renderOwnComponent() {
+    const {textColor, text} = this.props;
+    return <Text style={[androidStyle.text, {color: textColor}]}>{text}</Text>;
+  }
 
-	render() {
-		const {customComponent, onPress, style, showKnob} = this.props;
-		const animatedStyle = [androidStyle.notification,
-			style, {
-				top: this.offset,
-				transform: [{translateY: this.translateY}],
-        borderRadius: 10,
+  render() {
+
+    const {
+      customComponent,
+      onPress,
+      style,
+    } = this.props;
+
+    const animatedStyle = [
+      androidStyle.notification,
+      style,
+      {
+        top: this.offset,
+        transform: [{translateY: this.translateY}],
         backgroundColor: `rgba(255,255,255,${this.props.blurAmount/100})`
-			}];
-		return (
-			<Fragment>
-				<PanGestureHandler onHandlerStateChange={this.onHandlerStateChange} onGestureEvent={this.onGestureEvent}>
-					<Animated.View onLayout={this.handleOnLayout} style={animatedStyle}>
-						<TouchableOpacity style={[androidStyle.container]} activeOpacity={1} onPress={onPress}>
-							<View style={androidStyle.content}>
-								{customComponent ? this.renderCustomComponent() : this.renderOwnComponent()}
+      },
+    ];
+
+    return (
+      <Fragment>
+        <PanGestureHandler
+          onHandlerStateChange={this.onHandlerStateChange}
+          onGestureEvent={this.onGestureEvent}>
+
+          <Animated.View
+            onLayout={this.handleOnLayout}
+            style={animatedStyle}>
+
+            <TapGestureHandler
+              style={androidStyle.container}
+              onHandlerStateChange={this.onTapHandlerStateChange}>
+              <View style={androidStyle.content}>
+                {customComponent ? customComponent : this.renderOwnComponent()}
                 {showKnob &&
                 <View style={ androidStyle.knob } />
-                }
-							</View>
-						</TouchableOpacity>
-					</Animated.View>
-				</PanGestureHandler>
-			</Fragment>
-		)
-	}
+              </View>
+            </TapGestureHandler>
+
+          </Animated.View>
+
+        </PanGestureHandler>
+      </Fragment>
+    );
+  }
 }
