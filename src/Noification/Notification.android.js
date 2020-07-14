a2fe9aee3d4fd0cac5b5aafdc43da51a71538133
vuongductuanktmt <anhtuanpro312@gmail.com>
2020-07-14 22:39:07 +0700

    Update Notification.android.js

--- src/Noification/Notification.android.js
+++ src/Noification/Notification.android.js
@@ -1,19 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from 'react'
 import {
   Animated,
   View,
   Text,
   StatusBar
-} from 'react-native';
+} from 'react-native'
 import {
   PanGestureHandler,
   TapGestureHandler
-} from 'react-native-gesture-handler';
-import { NotificationBase } from './NotificationBase';
-import { androidStyle } from "./androidStyle";
+} from 'react-native-gesture-handler'
+import { NotificationBase } from './NotificationBase'
+import { androidStyle } from './androidStyle'
 
-const MIN_VELOCITY_TO_FLING = -350;
-const BOUNCE_OFFSET = 150;
+const MIN_VELOCITY_TO_FLING = -350
+const BOUNCE_OFFSET = 150
 
 export class Notification extends NotificationBase {
 
@@ -21,59 +21,56 @@ export class Notification extends NotificationBase {
     duration: 2000,
     autohide: true,
     showKnob: true,
-  };
-
-  offset = StatusBar.currentHeight;
-
-  onHandlerStateChange = (event) => {
-    const {velocityY, translationY, numberOfPointers, state} = event.nativeEvent;
-
-    if (this.props.onDragGestureHandlerStateChange) {
-      this.props.onDragGestureHandlerStateChange(event);
-    }
-
-    if (state === 5) {
-      if (velocityY < MIN_VELOCITY_TO_FLING && numberOfPointers === 1) {
-        Animated.spring(this.translateY, {
-          toValue: (this.viewHeight + BOUNCE_OFFSET + this.offset * 2) * -1,
-          useNativeDriver: true,
-          velocity: velocityY,
-        }).start();
-        return;
-      }
-
-      if (translationY > ((this.viewHeight / 2) * -1)) {
-        this.show();
-      } else {
-        this.hide();
-      }
-    }
-  };
-
-  renderOwnComponent() {
-    const {textColor, text} = this.props;
-    return <Text style={[androidStyle.text, {color: textColor}]}>{text}</Text>;
   }
 
-  render() {
+  offset = StatusBar.currentHeight
+
+  onHandlerStateChange = (event) => {
+    const { velocityY, translationY, numberOfPointers, state } = event.nativeEvent
+
+    if (this.props.onDragGestureHandlerStateChange) {
+      this.props.onDragGestureHandlerStateChange(event)
+    }
+
+    if (velocityY < MIN_VELOCITY_TO_FLING && numberOfPointers === 1) {
+      Animated.spring(this.translateY, {
+        toValue: (this.viewHeight + BOUNCE_OFFSET + this.offset * 2) * -1,
+        useNativeDriver: true,
+        velocity: velocityY,
+      }).start()
+      return
+    }
+    if (translationY > ((this.viewHeight / 2) * -1)) {
+      this.show()
+    } else {
+      this.hide()
+    }
+  }
+
+  renderOwnComponent () {
+    const { textColor, text } = this.props
+    return <Text style={[androidStyle.text, { color: textColor }]}>{text}</Text>
+  }
+
+  render () {
 
     const {
       customComponent,
       onPress,
       style,
       showKnob
-    } = this.props;
+    } = this.props
 
     const animatedStyle = [
       androidStyle.notification,
       style,
       {
         top: this.offset,
-        transform: [{translateY: this.translateY}],
+        transform: [{ translateY: this.translateY }],
         borderRadius: 10,
-        backgroundColor: `rgba(255,255,255,${this.props.blurAmount/100})`
+        backgroundColor: `rgba(255,255,255,${this.props.blurAmount / 100})`
       },
-    ];
+    ]
 
     return (
       <Fragment>
@@ -90,7 +87,7 @@ export class Notification extends NotificationBase {
               onHandlerStateChange={this.onTapHandlerStateChange}>
               <View style={androidStyle.content}>
                 {customComponent ? customComponent : this.renderOwnComponent()}
-                {showKnob && <View style={ androidStyle.knob } />}
+                {showKnob && <View style={androidStyle.knob}/>}
               </View>
             </TapGestureHandler>
 
@@ -98,6 +95,6 @@ export class Notification extends NotificationBase {
 
         </PanGestureHandler>
       </Fragment>
-    );
+    )
   }
 }
